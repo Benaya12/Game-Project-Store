2a94815a77251d72f6b91cbc905ab63c60d94a76
Benaya12 <102980424+Benaya12@users.noreply.github.com>
2025-02-03 21:22:15 +0000

    admin_model

--- Library-Project-main/backend/models/admin.py
+++ Library-Project-main/backend/models/admin.py
@@ -1,8 +1,7 @@
 # create a __init__.py file in the models folder to make all the model act as a package if you dont have it already , also you can make one file with all the models just copy ythe all the code in the files into it
 from . import db
 
-class User(db.Model):
+class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
-
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
