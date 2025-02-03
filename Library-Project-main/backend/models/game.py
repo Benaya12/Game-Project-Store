1565141db5a03549712c29c14c0234e9fb487be6
Benaya12 <102980424+Benaya12@users.noreply.github.com>
2025-02-03 21:40:22 +0000

    models

--- Library-Project-main/backend/models/game.py
+++ Library-Project-main/backend/models/game.py
@@ -3,10 +3,11 @@
 from . import db
 
 
+# models/game.py
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    genre = db.Column(db.String(200), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    loan_status = db.Column(db.Integer, nullable=False)
-    
+    title = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    loan_status = db.Column(db.Boolean, default=False)  # Indicates if the game is currently loaned
